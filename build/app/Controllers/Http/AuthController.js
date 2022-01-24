723873cd3d2ef000f5c43e2f97d016c2100a5af5
mee <baramee1407@gmail.com>
2022-01-25 02:38:56 +0700

    use build directly

---
+++ build/app/Controllers/Http/AuthController.js
@@ -0,0 +1,37 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const users_1 = __importDefault(global[Symbol.for('ioc.use')]("App/Models/users"));
+const Users = new users_1.default();
+class AuthController {
+    async register({ view }) {
+        return view.render('user/create');
+    }
+    async login({ view }) {
+        return view.render('login');
+    }
+    async store({ request, response }) {
+        const data = request.only(['email', 'password']);
+        Users.email = data.email;
+        Users.password = data.password;
+        await Users.save();
+        return response.redirect('/login');
+    }
+    async authenticate({ auth, request, response }) {
+        const email = request.input('email');
+        const password = request.input('password');
+        const rememberMe = request.input('rememberMe');
+        try {
+            await auth.use('web').attempt(email, password, rememberMe);
+            response.redirect('/hello');
+        }
+        catch {
+            return response.badRequest('Invalid credentials');
+        }
+        return response.redirect('/home');
+    }
+}
+exports.default = AuthController;
+//# sourceMappingURL=AuthController.js.map
---
+++ build/start/routes.js
@@ -0,0 +1,48 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Route_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Core/Route"));
+const Database_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Lucid/Database"));
+const luxon_1 = require("luxon");
+Route_1.default.get('/', (ctx) => {
+    ctx.response.redirect().status(301).toPath('/home');
+});
+Route_1.default.get('/leaders', async () => {
+    const leaders = await Database_1.default.query().from('leaders').select('*');
+    console.log(leaders);
+    return leaders;
+});
+Route_1.default.get('/home', async ({ view }) => {
+    const unsubmitLeaders = await Database_1.default.query().from('leaders').where('isSubmit', false);
+    const submitLeaders = await Database_1.default.query().from('leaders').where('isSubmit', true);
+    const html = await view.render('home', {
+        unsubmitUsers: unsubmitLeaders,
+        submitUsers: submitLeaders,
+    });
+    return html;
+});
+Route_1.default.post('/home', async ({ request, response }) => {
+    const id = request.input('id');
+    const submit = request.input('submit');
+    let time = luxon_1.DateTime.now();
+    if (submit) {
+        await Database_1.default.from('leaders').where('id', id).update({ isSubmit: submit, submitTime: time });
+    }
+    else {
+        await Database_1.default.from('leaders').where('id', id).update({ isSubmit: submit, submitTime: null });
+    }
+    return response.redirect().status(301).toPath('/home');
+});
+Route_1.default.post('/clear', async ({ auth, response }) => {
+    await auth.use('web').authenticate();
+    console.log(auth.use('web').user);
+    await Database_1.default.from('leaders').update({ isSubmit: false, submitTime: null });
+    return response.redirect().status(301).toPath('/home');
+});
+Route_1.default.get('/register', 'AuthController.register');
+Route_1.default.post('/register', 'AuthController.store');
+Route_1.default.get('/login', 'AuthController.login');
+Route_1.default.post('/login', 'AuthController.authenticate');
+//# sourceMappingURL=routes.js.map
